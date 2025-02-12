c236559a6551313b61a2f8ce8e8277278f207a04
NESARAJ GODWIN <103053734+MasterGodwin@users.noreply.github.com>
2025-02-12 13:52:47 +0530

    Update delete_user.php

--- stockmaintenance5/user/delete_user.php
+++ stockmaintenance5/user/delete_user.php
@@ -7,7 +7,7 @@ ini_set('error_log', '../logs/error_log.txt');
 
 // Allow CORS for all origins
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, DELETE");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Content-Type: application/json");
 
